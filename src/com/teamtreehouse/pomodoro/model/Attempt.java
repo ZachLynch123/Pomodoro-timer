32a4bdb0558eb45b68dfc08ee4bc79d9ab543ede
Unknown <Zachary.Lynch1337@gmail.com>
2018-03-09 21:29:18 -0800

    Set up model for the timer

--- src/com/teamtreehouse/pomodoro/model/Attempt.java
+++ src/com/teamtreehouse/pomodoro/model/Attempt.java
@@ -3,5 +3,28 @@ package com.teamtreehouse.pomodoro.model;
 public class Attempt {
     private String mMessage;
     private int mRemaningSeconds;
+    private AttemptKind mKind;
 
+
+    public Attempt(String message, AttemptKind kind) {
+        mMessage = message;
+        mKind = kind;
+        mRemaningSeconds = kind.getTotalSeconds();
+    }
+
+    public String getmMessage() {
+        return mMessage;
+    }
+
+    public int getmRemaningSeconds() {
+        return mRemaningSeconds;
+    }
+
+    public AttemptKind getmKind() {
+        return mKind;
+    }
+
+    public void setmMessage(String mMessage) {
+        this.mMessage = mMessage;
+    }
 }
